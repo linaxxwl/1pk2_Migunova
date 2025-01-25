93ab4d5293f6fb47cf1be18a9102da28f5adc064
User <User@WIN-3O6JK09Q9QF>
2025-01-26 00:39:24 +0300

    Добавьте файлы проекта.

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line; 
+            int lineCount = 0; 
+
+            while (true)
+            {
+                Console.WriteLine("Введите текст или 'exit':");
+                line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+
+                lineCount++;
+            }
+
+            Console.WriteLine($"Количество введенных строк: {lineCount}");
+
+        }
+    }
+}
