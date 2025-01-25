f569458b6d7c7aab1f7bff6fef31cd26f853c6b9
migunli@mail.ru <migunli@mail.ru>
2025-01-26 01:43:32 +0300

    выполнена 8 задача 3 урока

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_03_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short degrees = 0;
+
+            {
+                Console.Write(" Введите любую тмепературу в градусах цельсия ");
+                Console.Write
+            }
+        }
+    }
+}
---
+++ Task_03_08/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 20; i <= 50; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
