e75a07176ddbf84f217cf92df7aafbbd824388f2
migunli@mail.ru <migunli@mail.ru>
2025-01-26 15:40:42 +0300

    выполнена 5 задача 4 урока

---
+++ Task_04_05/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_04_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rainfall = new int[30];
+            Random random = new Random();
+            int[] decadalRainfall = new int[3]; //суммп осадков
+
+            int maxRainfall = 0;
+            int maxRainfallDay = 0; 
+
+           
+            for (int i = 0; i < 30; i++)
+            {
+                rainfall[i] = random.Next(0, 301); 
+
+               
+                int decadalIndex = i / 10;
+                decadalRainfall[decadalIndex] += rainfall[i]; //складываю осадки за день
+
+                
+                if (rainfall[i] > maxRainfall)
+                {
+                    maxRainfall = rainfall[i];
+                    maxRainfallDay = i + 1; 
+                }
+            }
+
+            Console.WriteLine(" Сумма осадков ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Декада {i + 1}: {decadalRainfall[i]} ");
+            }
+
+            Console.WriteLine($"День с самыми сильными осадками: {maxRainfallDay} (осадки: {maxRainfall} )");
+
+            Console.WriteLine("Дни без осадков:");
+            for (int i = 0; i < 30; i++)
+            {
+                if (rainfall[i] == 0)
+                {
+                    Console.WriteLine($"День {i + 1}");
+                }
+            }
+        }
+    }
+}
