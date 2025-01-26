98434d467df24d997ee9e64c5f5b075dc1053ce8
migunli@mail.ru <migunli@mail.ru>
2025-01-26 12:52:14 +0300

    выполнена 7 задача 3 урока

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double g = 9.8;
+            double t = 0;
+            double maxt = 5;
+
+            Console.WriteLine(" Время\tСкорость ");
+            Console.WriteLine();
+
+            while (t <= maxt)
+            {
+                double v = g * t;
+                Console.WriteLine($" {t,-12} {v:F2} ");
+                t += 0.5;
+            }
+        }
+    }
+}
