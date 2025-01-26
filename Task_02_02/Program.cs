67050dc5c48c715155cd73f6d86ad01cc73ffa06
migunli@mail.ru <migunli@mail.ru>
2025-01-26 18:19:20 +0300

    выполнена 2 задача 2 урока

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4;
+
+            double result =
+                Math.Pow(b + Math.Pow((a - 1), 1 / 3), 0.25) /
+                (Math.Abs(a - b) * (Math.Pow(Math.Sin(c), 2) + Math.Tan(c)));
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
