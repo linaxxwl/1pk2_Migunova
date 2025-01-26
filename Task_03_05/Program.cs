78c1aa15214e8b3ef17888351dfd41d6ffe4affd
migunli@mail.ru <migunli@mail.ru>
2025-01-26 11:46:04 +0300

    выполнена 5 задача 3 урока

--- Task_03_05/Program.cs
+++ Task_03_05/Program.cs
@@ -1,14 +1,29 @@
-﻿namespace Task_03_05
+﻿using System;
+using System.Data;
+
+namespace Task_03_05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+        
+        Console.Write(" Начальная температура в градусах Цельсия ");
+        double start = Convert.ToDouble(Console.ReadLine());
 
-            {
-               
-            }
+        Console.Write(" Конечная температура в градусах Цельсия ");
+        double end = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Цельсия\tФаренгейт");
+            
+            
+        for (double C = start; C <= end; C++)
+            
+        {
+             double F = C * 1.8 + 32;
+             Console.WriteLine($"{C}\t{F}"); 
+        }
+
         }
     }
 }
