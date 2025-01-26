f06c374db141e9d3347a6b5ba68bd92c15240fc7
migunli@mail.ru <migunli@mail.ru>
2025-01-26 19:19:50 +0300

    выполнена 8 задача 4 урока

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_04_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 50;
+            int[] array = new int[size];
+            Random rnd = new Random();
+            int pairCount = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(0, 50);
+                Console.WriteLine(array[i]);
+            }
+
+            for (int i = 0; i < size; i++)//пары чисел
+
+            {
+                for (int j = 0; j < i; j++)
+
+                {
+                    if (array[i] == array[j])
+                    {
+                        pairCount++;
+                        Console.WriteLine($" Пара: {array[i]}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Количество пар чисел: {pairCount}");
+        }
+    }
+}
