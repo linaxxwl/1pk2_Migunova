5c931d1a85bcb4e67b3ee3378bbce0e308f74f12
migunli@mail.ru <migunli@mail.ru>
2025-01-27 07:39:08 +0300

    дорешать задачу

---
+++ Task_04_06/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_04_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] numbers = new int size;
+            
+        }
+    }
+}
