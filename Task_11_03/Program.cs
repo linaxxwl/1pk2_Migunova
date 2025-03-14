42fb0deab5e5cbe7cd707ee449ca8b267c507060
linaxxwl <migunova.polina08@gmail.com>
2025-03-14 23:04:40 +0300

    сделано

--- Task_11_03/Program.cs
+++ Task_11_03/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" введите буквы :");
+            string input = Console.ReadLine();
+
+            int vowelsCount, consonantsCount;
+
+            // вызываю метод с выводом
+            CountVowelsAndConsonants(input, out vowelsCount, out consonantsCount);
+
+            Console.WriteLine(" количество гласных: " + vowelsCount);
+            Console.WriteLine(" количество согласных: " + consonantsCount);
+        }
+
+        static void CountVowelsAndConsonants(string str, out int vowels, out int consonants)
+        {
+            vowels = 0;
+            consonants = 0;
+
+            char[] vowelsArray = { 'у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю', 'У', 'Е', 'Ы', 'А', 'О', 'Я', 'Э', 'И', 'Ю' };
+
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c)) 
+                {
+                    if (Array.Exists(vowelsArray, v => v == c)) // 
+                    {
+                        vowels++;
+                    }
+                    else
+                    {
+                        consonants++;
+                    }
+                }
+            }
         }
     }
 }
+    
+
